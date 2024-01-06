2ef1c806d2c298b4d2dcdc80c44c17712f1ebb23
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2024-01-05 21:27:54 -0300

    chore: add APP_BASE_URL enviroment at the required files

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -5,7 +5,7 @@ export const envSchema = z.object({
     .string()
     .default('postgresql://docker:docker@localhost:5432/shorten-url'),
   JWT_SECRET: z.string().default('secret'),
-  APP_BASE_URL: z.string(),
+  APP_BASE_URL: z.string().default('http://localhost:3000'),
 });
 
 export type Env = z.infer<typeof envSchema>;
